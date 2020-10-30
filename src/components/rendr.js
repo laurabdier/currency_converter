0feff9a95dd965a3ab1c552a46049cd1d987330a
laurabdier <laura.boudier@coding-academy.fr>
2020-10-30 21:03:20 +0200

    added final button to call to action

--- src/components/rendr.js
+++ src/components/rendr.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react"
 import { observer } from "mobx-react"
 
 import Loader from './Loader'
@@ -13,7 +13,7 @@ const App = observer(({ brain }) => {
     }, [brain]);
 
     if (brain.state === "loading") {
-        return <Loader/>
+        return <Loader />
     }
     else if (brain.state === "error") {
         return <p>error while fetching API.</p>
@@ -30,43 +30,28 @@ const App = observer(({ brain }) => {
                         <div className="col-5 offset-1 d-flex justify-content-left">
                             <input className="convertBoxDate" type="date" max={today} value={brain.date} onChange={event => brain.setDate(event)}></input>
                         </div>
-                        
 
                         <div className="col-5 d-flex justify-content-center">
-                            <div className="row" style={{width: "80%"}}>
-                                <div className="col-sm-5 justify-content-left">
-                                <p className="convertBoxtext"> 1 {brain.currency1} </p>
-                                </div>
-                                <div className="col-sm-2 justify-content-left">
-                                <p>  <img src="/right.png" className="imgArrow" alt="arrow"></img> </p>
-                                </div>
-                                <div className="col-sm-5 justify-content-right">
-                                <p className="convertBoxtext"> {brain.todayRate} {brain.currency2}</p>
-                                </div>
-                            </div>
+                            <p className="convertBoxtext"> 1 {brain.currency1} equals {brain.todayRate} {brain.currency2}</p>
                         </div>
-
                     </div>
-
-
 
                     <div className="row " style={{ marginTop: 50 }}>
                         <div className="col-5 offset-1 d-flex justify-content-left">
                             <input className="convertBoxNumber" type="number" value={brain.amount} onChange={event => brain.setAmount(event)}></input>
                         </div>
                         <div className="col-5 d-flex justify-content-center">
-                                    <select className="convertBoxCurrency" value={brain.currency1} onChange={event => brain.setCurrency1(event)}>
-                                        {
-                                            brain.currencies.map((currency, index) => (
-                                                <option key={index} value={currency}>{currency}</option>
-                                            ))
-                                        }
-                                    </select>
-
+                            <select className="convertBoxCurrency" value={brain.currency1} onChange={event => brain.setCurrency1(event)}>
+                                {
+                                    brain.currencies.map((currency, index) => (
+                                        <option key={index} value={currency}>{currency}</option>
+                                    ))
+                                }
+                            </select>
                         </div>
                     </div>
 
-                    <div className="row" style={{width: "100%", marginTop: 50 }}>
+                    <div className="row" style={{ width: "100%", marginTop: 50 }}>
                         <div className="col-12 d-flex justify-content-center" >
                             <span type="button" onClick={_ => brain.convert()}> <img src="/down.png" className="imgArrowDown" alt="arrow"></img>  </span>
                         </div>
@@ -77,13 +62,19 @@ const App = observer(({ brain }) => {
                             <div className="convertBoxResult"><p className="result">{brain.result}</p></div>
                         </div>
                         <div className="col-5 d-flex justify-content-center">
-                                    <select className="convertBoxCurrency" value={brain.currency2} onChange={event => brain.setCurrency2(event)}>
-                                        {
-                                            brain.currencies.map((currency, index) => (
-                                                <option key={index} value={currency}>{currency}</option>
-                                            ))
-                                        }
-                                    </select>
+                            <select className="convertBoxCurrency" value={brain.currency2} onChange={event => brain.setCurrency2(event)}>
+                                {
+                                    brain.currencies.map((currency, index) => (
+                                        <option key={index} value={currency}>{currency}</option>
+                                    ))
+                                }
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="row" style={{marginTop: 50, width: "100%"}}>
+                        <div className="col-10 offset-1 justify-content-center">
+                            <button type="button" className="convertBoxButton"> CONVERT</button>
                         </div>
                     </div>
                 </div>
